e8f0ed3b9252964ac5ea53d01bb0a75edc3912c2
SubhoPaul2004 <paulsubhojit2004@gmail.com>
2026-04-24 00:07:04 +0530

    Added vercel.json

--- server/server.js
+++ server/server.js
@@ -51,3 +51,9 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+const cors = require('cors');
+app.use(cors({
+  origin: true, 
+  credentials: true
+}));
